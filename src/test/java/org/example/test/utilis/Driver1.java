56bab9fddd3e5eecfb1111edfdc3eba2bf71b304
ionut34x <ionut34x@gmail.com>
2025-11-15 14:43:38 +0200

    fix: updated selenoid config

--- src/test/java/org/example/test/utilis/Driver1.java
+++ src/test/java/org/example/test/utilis/Driver1.java
@@ -1,28 +1,18 @@
 package org.example.test.utilis;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Driver1 {
 
     public static WebDriver setup() {
         ChromeOptions options = new ChromeOptions();
 
-        // Capabilități necesare pentru Selenoid
-        options.setCapability("enableVNC", true);
-        options.setCapability("enableVideo", true);
+        // Pentru local
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--start-maximized");
 
-        try {
-            return new RemoteWebDriver(
-                    new URL("http://localhost:4444/wd/hub"),
-                    options
-            );
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Selenoid URL invalid!", e);
-        }
+        return new ChromeDriver(options);
     }
 }
--- src/test/java/org/example/test/utilis/DriverSelenoid.java
+++ src/test/java/org/example/test/utilis/DriverSelenoid.java
@@ -3,7 +3,6 @@ package org.example.test.utilis;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.ArrayList;
@@ -12,33 +11,22 @@ import java.util.HashMap;
 public class DriverSelenoid {
 
     public static WebDriver setup() throws MalformedURLException {
-
         ChromeOptions options = new ChromeOptions();
-
-        // IMPORTANT pentru Selenium 4 -> fără asta aruncă Illegal key values
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless=new");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--window-size=1920,1080");
-
-        // Specific pentru Selenoid
-        HashMap<String, Object> selenoidOptions = new HashMap<>();
-        selenoidOptions.put("name", "GitHub Actions Test");
-        selenoidOptions.put("sessionTimeout", "15m");
-        selenoidOptions.put("enableVNC", true);
-        selenoidOptions.put("enableVideo", true);
-        selenoidOptions.put("enableLog", true);
-        selenoidOptions.put("env", new ArrayList<String>() {{
-            add("TZ=UTC");
+        options.setCapability("browserVersion", "128.0");
+        options.setCapability("selenoid:options", new HashMap<String, Object>() {{
+            put("name", "GitHub Actions Test");
+            put("sessionTimeout", "15m");
+            put("env", new ArrayList<String>() {{
+                add("TZ=UTC");
+            }});
+            put("enableVideo", true);
+            put("enableVNC", true);
+            put("enableLog", true);
         }});
 
-        options.setCapability("browserVersion", "128.0");
-        options.setCapability("selenoid:options", selenoidOptions);
+        // IMPORTANT: NICI UN HEADLESS AICI!!
+        // Chrome trebuie să aibă interfață grafică pentru video.
 
-        // Conectare la Selenoid local din GitHub Actions
         return new RemoteWebDriver(
                 URI.create("http://127.0.0.1:4444/wd/hub").toURL(),
                 options
